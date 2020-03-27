3018baaf276adccb16c1b4550c49859efabaf8cc
Vinicius Mayer <viniciusmayer@gmail.com>
2020-03-27 16:13:53 -0300

    Habilitada a assinatura de um arquivo em especial - não necessariamente um timesheet

--- main.py
+++ main.py
@@ -1,63 +1,58 @@
 import glob, os, shutil, sys
-from tss.TimeSheetSigner import TimeSheetSigner
+from tss.TimeSheetSigner import TimeSheetSigner, FileSigner
 
 
 def ajuda():
     print('= HELP =')
     print('Command: python3 main.py [option]')
     print('Options:')
-    print('\t assinar')
-    print('\t mes=yyyymm')
-    print('\t separador=[-, _]. Default: -')
-    print('\t ajuda')
-    print('Example: python3 main.py assinar')
-    print('Example: python3 main.py assinar mes=201811')
-    print('Example: python3 main.py assinar mes=201811 separador=-')
+    print('\t arquivo=')
+    print('\t x=')
+    print('\t y=')
 
 
-def definir_ano_mes(mes, separador):
+def definir_ano_mes(arquivo):
     ano = None
-    _mes = None
+    mes = None
     ano_mes = None
-    if mes is not None:
-        ano = mes[0:4]
-        _mes = mes[4:6]
-        ano_mes = '{0}{1}{2}'.format(ano, separador, _mes)
-        print('ano_mes: {0}'.format(ano_mes))
-    return ano, _mes, ano_mes
+    if arquivo is not None:
+        ano = int(arquivo[13:17])
+        mes = int(arquivo[18:20])
+        ano_mes = '{0}{1}{2}'.format(ano, '-', mes)
+    return ano, mes, ano_mes
 
 
-def realizar_assinatura(origem, destino, assinatura, mes, separador, tmp):
-    ano, _mes, ano_mes = definir_ano_mes(mes, separador)
-    tss = TimeSheetSigner(assinatura, ano, _mes, tmp)
+def assinar_arquivos(origem, destino, assinatura, tmp):
     for arquivo in glob.iglob(origem + '**/*.pdf', recursive=True):
-        if (ano_mes is None) or (ano_mes in arquivo):
-            tss.assinar(arquivo, destino)
+        ano, mes, ano_mes = definir_ano_mes(arquivo)
+        tss = TimeSheetSigner(assinatura, tmp)
+        tss.assinar(arquivo, destino, ano, mes)
+
+
+def assinar_arquivo(origem, destino, assinatura, x, y, tmp):
+    tss = FileSigner(assinatura, tmp)
+    tss.assinar(origem, destino, x, y)
 
 
 def definir_entradas():
-    assinar = False
-    mes = None
-    separador = '-'
-    comandoChave, comandoValor = None, None
+    x, y, nome_arquivo = None, None, None
     for i in range(1, len(sys.argv)):
-        if sys.argv[i].find('=') > 1:
-            comandoChave, comandoValor = sys.argv[i].split('=')
+        comando = sys.argv[i]
+        if comando.find('=') >= 1:
+            comandoChave, comandoValor = comando.split('=')
         else:
             comandoChave = sys.argv[i]
 
-        if comandoChave == 'assinar':
-            assinar = True
-        elif comandoChave == 'mes':
-            mes = comandoValor
-        elif comandoChave == 'separador':
-            separador = comandoValor
-    return assinar, mes, separador
+        if comandoChave == 'arquivo':
+            nome_arquivo = comandoValor
+        elif comandoChave == 'x':
+            x = int(comandoValor)
+        elif comandoChave == 'y':
+            y = int(comandoValor)
+        else:
+            ajuda()
 
-
-def configurar(tmp):
-    if not os.path.exists(tmp):
-        os.makedirs(tmp)
+    return x, y, nome_arquivo
 
 
 def encerrar(tmp):
@@ -68,24 +63,20 @@ def encerrar(tmp):
             pass
 
 
-origem = 'files/pdf/'
-destino = 'files/pdf/'
+origem = 'files/origem/'
+destino = 'files/destino/'
 assinatura = 'files/assinatura.jpg'
 tmp = 'files/tmp/'
 if __name__ == '__main__':
     print()
     print('inicio')
-    configurar(tmp)
 
-    j = len(sys.argv)
-    if j > 1:
-        assinar, mes, separador = definir_entradas()
-        if assinar:
-            realizar_assinatura(origem, destino, assinatura, mes, separador, tmp)
-        else:
-            ajuda()
+    x, y, nome_arquivo = definir_entradas()
+    if nome_arquivo is not None:
+        origem = nome_arquivo
+        assinar_arquivo(origem, destino, assinatura, x, y, tmp)
     else:
-        ajuda()
+        assinar_arquivos(origem, destino, assinatura, tmp)
 
     encerrar(tmp)
     print('fim')
--- tss/TimeSheetSigner.py
+++ tss/TimeSheetSigner.py
@@ -7,38 +7,24 @@ from reportlab.pdfgen import canvas
 from calendar import monthrange
 
 
-class TimeSheetSigner(object):
+class Signer(object):
 
-    def __init__(self, assinatura, ano, mes, tmp):
-        self.ano = int(ano) if ano is not None else None
-        self.mes = int(mes) if mes is not None else None
+    def __init__(self, assinatura, tmp):
         self.assinatura = assinatura
         self.tmp = tmp
         if not os.path.exists(self.tmp):
             os.makedirs(self.tmp)
 
-    def definir_x_y(self, largura):
-        first_weekday, dias = None, 30
-        if self.ano is not None and self.mes is not None:
-            first_weekday, dias = monthrange(self.ano, self.mes)
-        x = largura - (11.5 * cm)
-        y = (7.25 * cm)
-        if dias in range(28, 29):
-            y = (7.5 * cm)
-        elif dias == 31:
-            y = (7 * cm)
-        return x, y
-
-    def gerar_pagina_assinada(self, largura, altura):
+    def gerar_pagina_assinada(self, largura, altura, a, b):
         arquivo = '{0}{1}'.format(self.tmp, str(uuid.uuid4()))
         c = canvas.Canvas(arquivo, pagesize=(largura, altura))
-        x, y = self.definir_x_y(largura)
+        x, y = self.definir_x_y(largura, a, b)
         c.drawImage(self.assinatura, x, y, width=(1.5 * cm), height=(1.5 * cm))
         c.showPage()
         c.save()
         return PdfFileReader(open(arquivo, 'rb')).getPage(0)
 
-    def assinar(self, arquivo, destino):
+    def assinar(self, arquivo, destino, a, b):
         nome_arquivo = arquivo[arquivo.rfind('\\') + 1:len(arquivo)]
         _arquivo = PdfFileReader(open(arquivo, 'rb'))
         if _arquivo.isEncrypted:
@@ -48,11 +34,40 @@ class TimeSheetSigner(object):
             pagina = _arquivo.getPage(0)
             largura = float(pagina.mediaBox.getWidth())
             altura = float(pagina.mediaBox.getHeight())
-            pagina_assinada = self.gerar_pagina_assinada(largura, altura)
+            pagina_assinada = self.gerar_pagina_assinada(largura, altura, a, b)
             pagina.mergePage(pagina_assinada)
             _destino = PdfFileWriter()
             _destino.addPage(pagina)
-            _nome_arquivo = nome_arquivo.replace(' ', '_')
-            with open('{0}{1}-{2}'.format(destino, 'assinado', _nome_arquivo), 'wb') as f:
+            _nome_arquivo = '{0}{1}-{2}'.format(destino, 'assinado', nome_arquivo.replace(' ', '_'))
+            with open(_nome_arquivo, 'wb') as f:
                 _destino.write(f)
                 print('arquivo gerado: {0}'.format(_nome_arquivo))
+
+
+class FileSigner(Signer):
+
+    def definir_x_y(self, largura, x, y):
+        _x = largura - (11.5 * cm)
+        _y = (6.75 * cm)
+        if x is not None:
+            _x = largura - (x * cm)
+        if y is not None:
+            _y = (y * cm)
+        return _x, _y
+
+
+class TimeSheetSigner(Signer):
+
+    def definir_x_y(self, largura, ano, mes):
+        first_weekday, dias = None, None
+        if ano is not None and mes is not None:
+            first_weekday, dias = monthrange(ano, mes)
+        x = largura - (11.5 * cm)
+        y = (6.75 * cm)
+        if dias == 28:
+            y = (7.75 * cm)
+        elif dias == 29:
+            y = (7.5 * cm)
+        elif dias == 30:
+            y = (7 * cm)
+        return x, y
